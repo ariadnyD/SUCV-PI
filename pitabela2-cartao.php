6b6f34708aedce5c1cc30d959518f1df36f35ed1
Ariadny <ariadny.santos@escolar.ifrn.edu.br>
2021-09-07 15:24:06 -0300

    modificações

--- pitabela2-cartao.php
+++ pitabela2-cartao.php
@@ -1,7 +1,7 @@
 <?php
 include("config.php");
 $codigo = $_GET['codigo'];
-$consulta = $conn->query("SELECT vac_nome, van_lote, van_data, enf_nome, van_ubs from tb_vacinacao join tb_vacinas on vac_codigo = van_vac_codigo join tb_enfermeiros on enf_codigo = van_enf_codigo JOIN tb_pacientes on pac_codigo = van_pac_codigo where pac_codigo = $codigo");
+$consulta = $conn->query("SELECT vac_nome, van_dose, van_lote, van_data, enf_nome, van_ubs from tb_vacinacao join tb_vacinas on vac_codigo = van_vac_codigo join tb_enfermeiros on enf_codigo = van_enf_codigo JOIN tb_pacientes on pac_codigo = van_pac_codigo where pac_codigo = $codigo");
 
 $consulta2 = $conn->query("SELECT * from tb_pacientes where pac_codigo = $codigo");
 $resultado2= $consulta2->fetch_assoc();
@@ -35,9 +35,10 @@ $resultado2= $consulta2->fetch_assoc();
 	    <table border="1">
 			<thead>
 		    	<tr>
-		    		<th colspan="5"> <?php echo $resultado2['pac_nome'];?>/<?php echo $resultado2['pac_cartsus'];?></th>
+		    		<th colspan="6"> <?php echo $resultado2['pac_nome'];?>/<?php echo $resultado2['pac_cartsus'];?></th>
 			    </tr>
 				    <td> VACINA </td>
+				    <td> DOSE </td>
 				    <td> LOTE </td>
 				    <td> DATA </td>
 				    <td> APLICADOR </td>
@@ -48,6 +49,7 @@ $resultado2= $consulta2->fetch_assoc();
 			<tbody>
 			    <tr> 
 			        <td id="vacina" onclick="location.href = 'pitabela3-vacinas.php';" style="cursor: hand;"><?php echo $resultado['vac_nome']; ?></td>
+			        <td><?php echo $resultado['van_dose'];?></td>
 				    <td><?php echo $resultado['van_lote'];?></td>
 				    <td><?php echo $resultado['van_data'];?></td>
 				    <td><?php echo $resultado['enf_nome'];?></td>
