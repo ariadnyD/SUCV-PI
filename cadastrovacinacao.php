f9f0a92178c3b7d97da31168e3591b531ccfa201
Eloisa <maria-eloisa-santos@outlook.com>
2021-09-09 18:16:27 -0300

    rm

--- cadastrovacinacao.php
+++ cadastrovacinacao.php
@@ -17,7 +17,6 @@ if(isset($_POST['codp'])){
 	<meta charset="utf-8">
 	<title>Cadastro de Vacinação</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="stylesheet" type="text/css" href="css/cadastros.css">
 	<link rel="icon" sizes="57x57" href="assets/logo/icon.png">
 </head>
--- cadastrovacinas.php
+++ cadastrovacinas.php
@@ -15,7 +15,6 @@ if(isset($_POST['nomevac'])){
 	<meta charset="utf-8">
 	<title>Cadastro de Vacinas</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="stylesheet" type="text/css" href="css/cadastros.css">
 	<link rel="icon" sizes="57x57" href="assets/logo/icon.png">
 </head>
