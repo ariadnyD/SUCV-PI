b5635757f1df8e4c0d782bc08c3a9e701775f634
ariadnyD <87425141+ariadnyD@users.noreply.github.com>
2021-09-10 10:45:33 -0300

    Update pitabela1-pessoas.php
    inferno

--- pitabela1-pessoas.php
+++ pitabela1-pessoas.php
@@ -7,7 +7,15 @@ if(isset($_GET['pessoa'])){
 	}else{
 		echo "Não foi possivel encontrar nada!";
 	}
-} 
+}
+if(isset($_GET['excluir'])){
+	$codigo = $_GET['excluir'];
+	if($consulta2 = $conn->query("DELETE from tb_pacientes where pac_codigo = $codigo")){
+		header("Location: pitabela1-pessoas.php");
+	} else {
+		header("Location: pitabela1-pessoas.php");
+	}
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -21,14 +29,14 @@ if(isset($_GET['pessoa'])){
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 		<link href="https://fonts.googleapis.com/css2?family=Noto+Serif:wght@400;700&display=swap" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css2?family=Mitr:wght@500&display=swap" rel="stylesheet">
-		<link rel="icon" sizes="57x57" href="assets/logo/icon.png">
+		<link rel="icon" sizes="57x57" href="imagens/aba/icon.png">
 
 		<script src="https://kit.fontawesome.com/0b07c232ad.js" crossorigin="anonymous"></script>
 	</head>
 	<body>
 		<header>
 			<div class="logo">
-				<img id = "logoimg" src="assets/logo/logo.png" alt="logo do site">
+				<img id = "logoimg" src="imagens/logo/logo.png" alt="logo do site">
 				<div class="logotxt">
 					<p> SUCV </p>
 				</div>
@@ -48,6 +56,7 @@ if(isset($_GET['pessoa'])){
 				    <td> NOME </td>
 				    <td> CARTÃO DO SUS </td>
 				    <td> DATA DE NASCIMENTO </td> 
+				    <td> </td>
 			    </tr>
 			</thead>
 <?php if(isset($_POST['pessoa'])){
@@ -61,6 +70,8 @@ if(isset($_GET['pessoa'])){
 				    <td id="cartao" onclick="location.href = 'pitabela2-cartao.php?codigo= <?php echo $resultado2['pac_codigo'];?>';"style="cursor: hand;" > <?php echo $resultado2['pac_nome']; ?> </td>
 				    <td> <?php echo $resultado2['pac_cartsus']; ?> </td>
 				    <td> <?php echo $resultado2['pac_dtnasc']; ?> </td>
+				    <td><a href="pacientes-editar.php?codigo=<?php echo $resultado2['pac_codigo']; ?>"><img src="assets/body/editar.png" width="16"></a>&nbsp;
+				    <a href="?excluir=<?php echo $resultado2['pac_codigo']; ?>" onclick="return confirm('Tem certeza?')"><img src="assets/body/excluir.png" width="16"></a></td>
 			    </tr>
 			</tbody>
 <?php }else {
@@ -73,6 +84,8 @@ if(isset($_GET['pessoa'])){
 				    <td id="cartao" onclick="location.href = 'pitabela2-cartao.php?codigo= <?php echo $resultado['pac_codigo'];?>';"style="cursor: hand;" > <?php echo $resultado['pac_nome']; ?> </td>
 				    <td> <?php echo $resultado['pac_cartsus']; ?> </td>
 				    <td> <?php echo $resultado['pac_dtnasc']; ?> </td>
+				    <td>&nbsp;<a href="pacientes-editar.php?codigo=<?php echo $resultado['pac_codigo']; ?>"><img src="assets/body/editar.png" width="16"></a> 
+				    <a href="?excluir=<?php echo $resultado['pac_codigo']; ?>" onclick="return confirm('Tem certeza?')"><img src="assets/body/excluir.png" width="16"></a></td>
 			    </tr>
 			</tbody>
  <?php }}?>
